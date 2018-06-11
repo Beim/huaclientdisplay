1195dcfbd956ac99562bf2be2e4c0ac80afea1b0
beim <beim2015@outlook.com>
2018-06-11 20:26:04 +0800

    修改/rank1展示样式

--- src/components/ProjRankPage1.js
+++ src/components/ProjRankPage1.js
@@ -53,25 +53,29 @@ class ProjRankPage1 extends Component {
     genGiftDivList() {
         const onlist = this.state.onlist
         const offlist = this.state.offlist
-        console.log(this.state)
         const projSpanWrapperStyle = {
             fontSize: '30px',
             marginTop: '50px',
         }
-        const projFisrtSpanWrapperStyle = {
-            fontSize: '50px',
-            marginTop: '20px',
+        const spanStyle1 = {
+            width: '150px',
+            minWidth: '150px',
+            display: 'inline-block',
+        }
+        const spanStyle2 = {
+            width: '200px',
+            minWidth: '200px',
+            display: 'inline-block',
         }
         const divList = []
         for (let idx in onlist) {
             let item = onlist[idx]
-            let style = divList.length === 0 ? projFisrtSpanWrapperStyle : projSpanWrapperStyle
             let length = divList.length
             divList.push(
-                <div style={style}>
-                    {/* <span className="proj-span">{length + 1}.</span> */}
-                    <span className="proj-span">{item.name}</span>
-                    <span className="proj-span">{parseS2M(item.duration)}</span>
+                <div style={projSpanWrapperStyle}>
+                    <span style={spanStyle1}>{parseInt(idx) === 0 ? "进行中：" : ""}</span>
+                    <span style={spanStyle2}>{item.name}</span>
+                    <span>{parseS2M(item.duration)}</span>
                 </div>
             )
         }
@@ -79,10 +83,10 @@ class ProjRankPage1 extends Component {
             let item = offlist[idx]
             let style = projSpanWrapperStyle
             divList.push(
-                <div style={style}>
-                    {/* <span className="proj-span">{length + 1}.</span> */}
-                    <span className="proj-span">{item.name}</span>
-                    <span className="proj-span">已完成</span>
+                <div style={projSpanWrapperStyle}>
+                    <span style={spanStyle1}>{""}</span>
+                    <span style={spanStyle2}>{item.name}</span>
+                    <span>已完成</span>
                 </div>
             )
         }
